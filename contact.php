e0c43b9fd8bad0f311f4a021278145c7f3480d2d
jeanpierrelam76 <jeanpierrelam31@gmail.com>
2024-03-15 15:22:39 +0100

    test fichier contact

--- contact.php
+++ contact.php
@@ -1,13 +1,38 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    
-    $email = $_POST["email"];
-    
 
-    // Display the submitted data
-    
-    echo "Email: " . $email . "<br>";
-    
+<?php
+
+//header('location: index.html' );
+
+$email= $_POST['email'];
+
+
+
+$host = "localhost";
+$dbname = "agences";
+$username = "root";
+$password = "";
+        
+$conn = new mysqli($host, $dbname, $username, $password );
+        
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
 }
+        
+$sql = "INSERT INTO agences (email)
+        VALUES (?)";
+
+$stmt = mysqli_stmt_init($conn);
+
+if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+ 
+    die(mysqli_error($conn));
+}
+
+$stmt->bind_param("ssis",$email);
+
+mysqli_stmt_execute($stmt);
+
+echo "email envoyé avec succés! <br>";
 ?>
